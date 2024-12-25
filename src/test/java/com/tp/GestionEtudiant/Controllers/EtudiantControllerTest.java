148eaa97d8a7795fde5174f29751e272f3356cb6
khalil abbaoui <khalil.abbaoui2002@gmail.com>
2024-12-25 01:01:00 +0100

    update pom.xml: add sruefie-plugin

--- src/test/java/com/tp/GestionEtudiant/Controllers/EtudiantControllerTest.java
+++ src/test/java/com/tp/GestionEtudiant/Controllers/EtudiantControllerTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,6 +20,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(EtudiantController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EtudiantControllerTest {
 
     @Autowired
